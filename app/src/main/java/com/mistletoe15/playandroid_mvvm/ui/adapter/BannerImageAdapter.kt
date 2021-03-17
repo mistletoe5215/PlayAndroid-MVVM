c901394f7bbbdb7e01b8912cd1c05287150e7eb7
wangzhiying <wangzhiying@bilibili.com>
2021-03-17 18:01:34 +0800

    some fix

--- app/src/main/java/com/mistletoe15/playandroid_mvvm/ui/adapter/BannerImageAdapter.kt
+++ app/src/main/java/com/mistletoe15/playandroid_mvvm/ui/adapter/BannerImageAdapter.kt
@@ -1,17 +1,18 @@
 package com.mistletoe15.playandroid_mvvm.ui.adapter
 import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.mistletoe.coroutinewrapper.acquireLifecycleScope
+import com.mistletoe.coroutinewrapper.acquireObservableScope
 import com.mistletoe15.playandroid_mvvm.data.bean.HomeBannerModel
 import com.mistletoe15.playandroid_mvvm.ui.activity.WebViewPage
 import com.mistletoe15.playandroid_mvvm.utils.ViewUtil.getHttpBitmap
-import com.mistletoe15.playandroid_mvvm.utils.doAsyncTasksThen2Main
 import com.youth.banner.adapter.BannerAdapter
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 /**
  * Created by Mistletoe on 2020/6/2
@@ -30,19 +31,18 @@ class BannerImageAdapter(dataList:List<HomeBannerModel>?) : BannerAdapter<HomeBa
     }
 
     override fun onBindView(holder: BannerViewHolder?, data: HomeBannerModel?, position: Int, size: Int) {
-        var bmp:Bitmap? = null
-        doAsyncTasksThen2Main(GlobalScope,{
-            bmp = getHttpBitmap(data?.imagePath)
-            bmp!=null
-        },mainBlock = {
-            (holder?.itemView as ImageView).apply {
-                setImageBitmap(bmp)
-                 val intent = Intent(context,  WebViewPage::class.java).apply {
-                     putExtra("url",data?.url)
-                 }
+        holder?.itemView?.context?.acquireLifecycleScope()?.launch {
+            val bmpDeferred  = async{
+                getHttpBitmap(data?.imagePath)
+            }
+            (holder.itemView as ImageView).apply {
+                setImageBitmap(bmpDeferred.await())
+                val intent = Intent(context,  WebViewPage::class.java).apply {
+                    putExtra("url",data?.url)
+                }
                 setOnClickListener{ startActivity(context,intent, Bundle())}
             }
-        })
+        }
     }
 
 }
